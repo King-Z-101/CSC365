dd4304c04f0aa3cb4e2fd65eefc2e139638234d3
sinchana2021 <sinchana.shivaprasad@gmail.com>
2024-09-28 16:48:32 -0700

    parsed commands

--- Lab1/schoolsearch.py
+++ Lab1/schoolsearch.py
@@ -18,7 +18,28 @@ def csv_parser():
         record = f.readlines()
         print(record)
 
-def parser_command(command):
+def parse_command(command):
+    if((command[0] == 'S') | (command[0] == 'Student:')):
+            if(len(command) == 3):
+                print("print the last name, first name and the bus route the student takes")
+            else:
+                print("print the last name, first name, grade and classroom assignment for each student found and the name of their teacher (last and first name)")
+    elif((command[0] == 'T') | (command[0] == 'Teacher:')): 
+        print("print the last and the first name of the student")
+    elif((command[0] == 'G') | (command[0] == 'Grade:')): 
+        if(len(command) == 3):
+            if((command[2] == 'H') | (command[2] == 'High')):
+                print("print info of student with highest GPA")
+            elif((command[2] == 'L') | (command[2] == 'Low')):
+                print("print info of student with lowest GPA")
+        else:
+            print("print the name (last and first) of the student")
+    elif((command[0] == 'B') | (command[0] == 'Bus:')): 
+        print("print the name of the student (last and first) and their grade and classroom")
+    elif((command[0] == 'A') | (command[0] == 'Average:')): 
+        print("print the grade level (the number provided in command) and the average GPA score rounded to two decimal places") 
+    elif((command[0] == 'I') | (command[0] == 'Info')): 
+        print("print the number of students in each grade in the format <Grade>: <Number of Students> sorted in ascending order by grade")    
     return
 
 
@@ -28,7 +49,9 @@ def main():
         user_input = input("Enter a command: ")
     #split the input into a list
         user_input_list = user_input.split()
-        parser_command(user_input_list)
+        if((user_input_list[0] == 'Q') | (user_input_list[0] == 'Quit')):
+            break
+        parse_command(user_input_list)
     print(user_input_list)
     #parser()
 
