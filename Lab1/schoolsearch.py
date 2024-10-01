3a1702f37b4235d93809680b65037d6bf87cad8d
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-09-30 23:00:45 -0700

    Implementation Done

--- Lab1/schoolsearch.py
+++ Lab1/schoolsearch.py
@@ -15,31 +15,53 @@ StLastName, StFirstName, Grade, Classroom, Bus, GPA, TLastName, TFirstName
 
 
 '''
+import sys
 
+#error messages
+error_MSG_INVALID_COMMAND = "Invalid Command"
+error_MSG_INVALID_COMMAND_FORMAT = "Invalid Command Format"
+error_MSG_NO_RECORD = "No Record(s) Found"
+error_MSG_INVALID_Grade = "Invalid Grade"
+error_MSG_INVALID_Bus = "Invalid Bus Route"
+
+#Note: grace will be added to passed names of students and teachers (case insensitive)
 def stundent_bus_finder(record, last_name):
+    count = 0
     for line in record:
         line = line.split(",")
-        if(line[0] == last_name):
-            print(line[0], line[1], line[4])
+        if(line[0] == last_name.upper()):
+            print(line[0] + ", "  + line[1] + ", "  + line[4])
+            count += 1
+    if(count == 0):
+        print(error_MSG_NO_RECORD)
     return
 
-
-def student_finder(record, student_name):
+def student_finder(record, last_name):
+    count = 0
     for line in record:
         line = line.split(",")
-        if(line[0] == student_name):
-            print(line[0], line[1], line[2], line[3], line[6], line[7])
+        if(line[0] == last_name.upper()):
+            print(line[0] + ", "  + line[1] + ", "  + line[2] + ", "  + line[3] + ", "  + line[6] + ", "  + line[7])
+            count += 1
+    if(count == 0):
+        print(error_MSG_NO_RECORD)
     return
 
 def teacher_finder(record, teacher_name):
+    count = 0
     for line in record:
         line = line.split(",")
-        if(line[6] == teacher_name):
-            print(line[0], line[1])
+        if(line[6] == teacher_name.upper()):
+            print(line[0] + ", "  + line[1])
+            count += 1
+    if(count == 0):
+        print(error_MSG_NO_RECORD)
     return
 
 def grade_max_student_finder(record, grade):
+    count = 0
     max_gpa = 0
+    #min_gpa = 0
     #first pass finds the max_gpa
     for line in record:
         line = line.split(",")
@@ -49,10 +71,14 @@ def grade_max_student_finder(record, grade):
     for line in record:
         line = line.split(",")
         if((line[2] == grade) & (float(line[5]) == max_gpa)):
-            print(line[0], line[1], line[5], line[6], line[7], line[4])
+            print(line[0] + ", "  + line[1] + ", "  + line[5] + ", "  + line[6] + ", "  + line[7] + ", "  + line[4])
+            count += 1
+    if(count == 0):
+        print(error_MSG_INVALID_Grade)
     return
 
 def grade_min_student_finder(record, grade):
+    count = 0
     min_gpa = 0
     #first pass finds the min_gpa
     for line in record:
@@ -63,21 +89,32 @@ def grade_min_student_finder(record, grade):
     for line in record:
         line = line.split(",")
         if((line[2] == grade) & (float(line[5]) == min_gpa)):
-            print(line[0], line[1], line[5], line[6], line[7], line[4])
+            print(line[0] + ", "  + line[1] + ", "  + line[5] + ", "  + line[6] + ", "  + line[7] + ", "  + line[4])
+            count += 1
+    if(count == 0):
+        print(error_MSG_INVALID_Grade)
     return
 
 def grade_student_finder(record, grade):
+    count = 0
     for line in record:
         line = line.split(",")
         if(line[2] == grade):
-            print(line[0], line[1])
+            print(line[0] + ", "  + line[1])
+            count += 1
+    if(count == 0):
+        print(error_MSG_INVALID_Grade)
     return
 
 def bus_student_finder(record, bus):
+    count = 0
     for line in record:
         line = line.split(",")
         if(line[4] == bus):
-            print(line[0], line[1], line[2], line[3])
+            print(line[0] + ", "  + line[1] + ", "  + line[2] + ", "  + line[3])
+            count += 1
+    if(count == 0):
+        print(error_MSG_INVALID_Bus)
     return
 
 def grade_level_average_finder(record, grade):
@@ -88,8 +125,12 @@ def grade_level_average_finder(record, grade):
         if(line[2] == grade):
             count += 1
             gpa_sum += float(line[5])
+    #check errors before printing (avoid division by zero)
+    if(count == 0):
+        print(error_MSG_INVALID_Grade)
+        return
     average_gpa = gpa_sum/count
-    print(grade + ":", round(average_gpa, 2))    
+    print(grade + ": " + f"{average_gpa:.2f}")    
     return
 
 def info_finder(record):
@@ -106,48 +147,53 @@ def info_finder(record):
     gradeList.sort()
     #get the value (number of students) for each grade
     for grade in gradeList:
-        print(grade + ":", grade_dict[grade])
+        print(grade + ": ", grade_dict[grade])
     return
 
 def parse_command(command):
-    #open students.txt file and pass to all helper functions
-    with open("students.txt") as f:
-        record = f.readlines()
+    #check if student.txt file exists
+    try:
+        #open students.txt file and pass to all helper functions
+        with open("students.txt") as f:
+            record = f.readlines()
 
-    if((command[0] == 'S') | (command[0] == 'Student:')):
+        if((command[0] == 'S:') | (command[0] == 'Student:')):
+                if(len(command) == 3):
+                    print("print the last name, first name and the bus route the student takes")
+                    stundent_bus_finder(record, command[1])
+                else:
+                    print("print the last name, first name, grade and classroom assignment for each student found and the name of their teacher (last and first name)")
+                    student_finder(record, command[1])
+
+        elif((command[0] == 'T:') | (command[0] == 'Teacher:')): 
+            print("print the last and the first name of the student")
+            teacher_finder(record, command[1])
+        elif((command[0] == 'G:') | (command[0] == 'Grade:')): 
             if(len(command) == 3):
-                print("print the last name, first name and the bus route the student takes")
-                stundent_bus_finder(record, command[1])
+                if((command[2] == 'H:') | (command[2] == 'High')):
+                    print("print info of student with highest GPA")
+                    grade_max_student_finder(record, command[1])
+                elif((command[2] == 'L:') | (command[2] == 'Low')):
+                    print("print info of student with lowest GPA")
+                    grade_min_student_finder(record, command[1])
+                    
             else:
-                print("print the last name, first name, grade and classroom assignment for each student found and the name of their teacher (last and first name)")
-                student_finder(record, command[1])
-
-    elif((command[0] == 'T') | (command[0] == 'Teacher:')): 
-        print("print the last and the first name of the student")
-        teacher_finder(record, command[1])
-    elif((command[0] == 'G') | (command[0] == 'Grade:')): 
-        if(len(command) == 3):
-            if((command[2] == 'H') | (command[2] == 'High')):
-                print("print info of student with highest GPA")
-                grade_max_student_finder(record, command[1], command[2])
-            elif((command[2] == 'L') | (command[2] == 'Low')):
-                print("print info of student with lowest GPA")
-                grade_min_student_finder(record, command[1], command[2])
-                
+                print("print the name (last and first) of the student")
+                grade_student_finder(record, command[1])
+        elif((command[0] == 'B:') | (command[0] == 'Bus:')): 
+            print("print the name of the student (last and first) and their grade and classroom")
+            bus_student_finder(record, command[1])
+        elif((command[0] == 'A:') | (command[0] == 'Average:')): 
+            print("print the grade level (the number provided in command) and the average GPA score rounded to two decimal places")
+            grade_level_average_finder(record, command[1]) 
+        elif((command[0] == 'I') | (command[0] == 'Info')): 
+            print("print the number of students in each grade in the format <Grade>: <Number of Students> sorted in ascending order by grade") 
+            info_finder(record)
         else:
-            print("print the name (last and first) of the student")
-            grade_student_finder(record, command[1])
-    elif((command[0] == 'B') | (command[0] == 'Bus:')): 
-        print("print the name of the student (last and first) and their grade and classroom")
-        bus_student_finder(record, command[1])
-    elif((command[0] == 'A') | (command[0] == 'Average:')): 
-        print("print the grade level (the number provided in command) and the average GPA score rounded to two decimal places")
-        grade_level_average_finder(record, command[1]) 
-    elif((command[0] == 'I') | (command[0] == 'Info')): 
-        print("print the number of students in each grade in the format <Grade>: <Number of Students> sorted in ascending order by grade") 
-        info_finder(record)   
-    return
-
+            print(error_MSG_INVALID_COMMAND)   
+        return
+    except FileNotFoundError:
+        sys.exit("Exiting Program: students.txt not found")
 
 def main():
     print("Hello, World!")
@@ -156,17 +202,10 @@ def main():
     #split the input into a list
         user_input_list = user_input.split()
         if((user_input_list[0] == 'Q') | (user_input_list[0] == 'Quit')):
+            print("Exiting Program")
             break
         parse_command(user_input_list)
-    print(user_input_list)
-    #parser()
+    return 
 
 if __name__ == "__main__":
     main()
-
-
-
-# def csv_parser():
-#     with open("students.txt") as f:
-#         record = f.readlines()
-#         print(record)
