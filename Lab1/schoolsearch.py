edf75245857b3245165eec63cb60cb0d31aa2669
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-10-04 18:26:15 -0700

    Done

--- Lab1/schoolsearch.py
+++ Lab1/schoolsearch.py
@@ -64,8 +64,8 @@ def classroom_teacher_finder(teachers, classroom):
     count = 0
     for line in teachers:
         line = line.split(",")
-        if(line[2] == classroom):
-            print(line[0] + ", "  + line[1] + ", "  + line[2])
+        if(line[2].strip() == classroom):
+            print(line[0] + ", "  + line[1] + ", "  + line[2].strip())
             count += 1
     if(count == 0):
         print(error_MSG_INVALID_Classroom)
@@ -83,7 +83,7 @@ def grade_teacher_finder(teachers, students, grade):
     for teacher in teachers:
         teacher = teacher.split(",")
         if teacher[2] in grade_classes:
-            print(teachers[0], teachers[1])
+            print(teachers[0], teachers[1].strip())
 
 #NR4
 def enrollment_finder(students):
@@ -126,7 +126,6 @@ def student_finder(record, teachers, last_name):
         print(error_MSG_NO_RECORD)
     return
 
-#TODO: find student based on teacher
 def teacher_finder(record, teachers, teacher_name):
     count = 0
     classroom = None
@@ -221,7 +220,86 @@ def grade_level_average_finder(record, grade):
         print(error_MSG_INVALID_Grade)
         return
     average_gpa = gpa_sum/count
-    print(grade + ": " + f"{average_gpa:.2f}")    
+    print(grade + ": " + f"{average_gpa:.2f}")    #eventually be removed 
+    return average_gpa
+
+def grade_gpa_average_finder(students): #find the avg. gpa of students for a corresponding grade level (lists.txt, index = 2)
+    '''
+        Grade: [sum_gpa, student count]
+    '''
+    grade_avg_gpa_map = {}
+    #first pass gets the grade levels, a gpa from a student of this grade, and student count of grade level
+    for student in students:
+        student = student.split(",")
+        grade, student_gpa = student[2], float(student[5])
+        if grade not in grade_avg_gpa_map:
+            grade_avg_gpa_map[grade] = [student_gpa, 1]
+        else:
+            grade_avg_gpa_map[grade][0] += student_gpa
+            grade_avg_gpa_map[grade][1] += 1
+        #sort the keys/grades, go through the dictionary to calculate the average gpa and report it.
+    #sort the dictionary by class number
+    gradeList = list(grade_avg_gpa_map.keys())
+    gradeList.sort()
+    #get the value (avg gpa of grade) for each grade
+    for g in gradeList:
+        grade_avg_gpa = grade_avg_gpa_map[g][0] / grade_avg_gpa_map[g][1]
+        print(g + ": " + f"{grade_avg_gpa:.2f}")   
+    return
+
+def teacher_gpa_average_finder(students, teachers): #find the average gpa for students of a corresponding teacher(s) (lists.txt, teachers.txt, index = 0, 1? (for teachers.txt))
+    teacher_classroom_map = {}
+
+    for line in teachers:
+        line = line.split(",")
+        teacher, classroom = line[0].strip(), line[2].strip()
+        if teacher not in teacher_classroom_map:
+            teacher_classroom_map[teacher] = [classroom]
+        else: #in our case this will never catch because each teacher only uses one classroom
+            teacher_classroom_map[teacher].append(classroom)
+
+    teacher_avg_gpa_map = {}
+    for student in students:
+        student = student.split(",")
+        classroom, student_gpa = student[3], float(student[5])
+        if classroom not in teacher_avg_gpa_map:
+            teacher_avg_gpa_map[classroom] = [student_gpa, 1]
+        else:
+            teacher_avg_gpa_map[classroom][0] += student_gpa
+            teacher_avg_gpa_map[classroom][1] += 1
+    classroomList = list(teacher_avg_gpa_map.keys())
+    classroomList.sort()
+    for c in classroomList:
+        classroom_avg_gpa = teacher_avg_gpa_map[c][0] / teacher_avg_gpa_map[c][1]
+
+        for teacher, t_classroom in teacher_classroom_map.items():
+            if c in teacher_classroom_map[teacher]:
+                print(teacher + ": " + f"{classroom_avg_gpa:.2f}")   
+    return
+
+#find the avg. gpa of students taken a corr. bus route. (lists.txt, index = 4)
+def bus_gpa_average_finder(students): 
+    '''
+        bus: [sum_gpa, student count]
+    '''
+    bus_avg_gpa_map = {}
+    #first pass gets the bus levels, a gpa from a student of this bus, and student count of bus level
+    for student in students:
+        student = student.split(",")
+        bus, student_gpa = student[4], float(student[5])
+        if bus not in bus_avg_gpa_map:
+            bus_avg_gpa_map[bus] = [student_gpa, 1]
+        else:
+            bus_avg_gpa_map[bus][0] += student_gpa
+            bus_avg_gpa_map[bus][1] += 1
+        #sort the keys/buss, go through the dictionary to calculate the average gpa and report it.
+    #sort the dictionary by class number
+    busList = list(bus_avg_gpa_map.keys())
+    busList.sort()
+    #get the value (avg gpa of bus) for each bus
+    for b in busList:
+        bus_avg_gpa = bus_avg_gpa_map[b][0] / bus_avg_gpa_map[b][1]
+        print(b + ": " + f"{bus_avg_gpa:.2f}")   
     return
 
 def info_finder(record):
@@ -254,18 +332,15 @@ def parse_command(command):
                 #check 3rd command to see if they want teacher or student info from the class
                 if((command[2] == 'S')):
                     classroom_student_finder(record, command[1]) #NR1*
-                if((command[2] == 'T')):
+                elif((command[2] == 'T')):
                     classroom_teacher_finder(teachers, command[1]) #NR2*
                 else:
                     print(error_MSG_INVALID_COMMAND_FORMAT)
-
-
         elif((command[0] == 'S:') | (command[0] == 'Student:')):
                 if(len(command) == 3):
                     stundent_bus_finder(record, command[1])
                 else:
                     student_finder(record, teachers, command[1])
-
         elif((command[0] == 'T:') | (command[0] == 'Teacher:')): 
             teacher_finder(record, teachers, command[1])
         elif((command[0] == 'G:') | (command[0] == 'Grade:')): 
@@ -275,19 +350,29 @@ def parse_command(command):
                 elif((command[2] == 'L') | (command[2] == 'Low')):
                     grade_min_student_finder(record, command[1])
                 elif(command[2] == 'T'): #NR3*
-                    grade_teacher_finder(teachers, record, command[1])
-                      
+                    grade_teacher_finder(teachers, record, command[1])              
             else:
                 grade_student_finder(record, command[1])
         elif((command[0] == 'B:') | (command[0] == 'Bus:')): 
             bus_student_finder(record, command[1])
         elif((command[0] == 'A:') | (command[0] == 'Average:')): 
-            grade_level_average_finder(record, command[1]) 
+            #grade_gpa_average_finder(record, command[1]) #NR5
+            grade_level_average_finder(record, command[1]) #DONT change this yet
         elif((command[0] == 'I') | (command[0] == 'Info')): 
             info_finder(record)
         elif((command[0] == 'E') | (command[0] == 'Enrollment')):  #NR4*
             enrollment_finder(record)
-
+        #NR5 commands (Data: Grade (G), Teacher (T), Bus (B))
+        elif((command[0] == 'Data') and (len(command) == 2)):
+            #check which relationship user want to analyze
+            if (command[1] == "G"):
+                grade_gpa_average_finder(record) #no -1 one grade, means users wants to look at all grades' avg gpa
+            elif (command[1] == "T"):
+                teacher_gpa_average_finder(record, teachers)
+            elif (command[1] == "B"):
+                bus_gpa_average_finder(record)
+            else:
+                print(error_MSG_INVALID_COMMAND_FORMAT)
         else:
             print(error_MSG_INVALID_COMMAND)   
         return
